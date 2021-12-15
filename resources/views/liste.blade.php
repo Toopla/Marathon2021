d02bbd0d157fcadbb193193ebebc9f3883186157
Patrickplouvin <Patrick.Plouvinpro@gmail.com>
2021-12-16 00:32:46 +0100

    " detail serie + href image "

--- resources/views/liste.blade.php
+++ resources/views/liste.blade.php
@@ -9,7 +9,7 @@
         <div class="recherchelogo">
             <a href="{{route('accueil')}}"><img src="img/logo.png"></a>
             <form>
-                <input id="recherche"  type="text"
+                <input id="recherche" type="text"
                        name="Recherche" placeholder="Rechercher un film/série">
                 <input type="submit" name="Rechercher" value="">
             </form>
@@ -23,7 +23,7 @@
 @if(!empty($séries))
     <ul>
         @foreach($séries as $série)
-            <a href=""><img src="{{$série->urlImage}}"></a>
+            <a href={{route('id',$série->id)}}><img src="{{$série->urlImage}}"></a>
         @endforeach
     </ul>
 
